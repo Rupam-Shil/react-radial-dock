5a34aba49eabd75e3a37e3b351dec655d22db92a
Rupam Shil <rupamshil111@gmail.com>
2026-04-29 19:02:30 +0530

    feat: public barrel

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-export {};
+export { default, RadialDock } from './RadialDock';
+export type {
+  RadialDockProps,
+  RadialDockItem,
+  RadialDockTheme,
+  RadialDockClassNames,
+  RadialDockAnimation,
+  RadialDockAnimationName,
+  RadialDockAnimationCustom,
+  RadialDockAnimationContext,
+  RadialDockHandle,
+  RadialDockReducedMotion,
+  RadialDockTriggers,
+} from './types';
